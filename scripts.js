732941fd83a9ddee49142147fb6a6eacddad954a
DavidAdam1323 <davidadamgtr@gmail.com>
2024-05-27 09:31:34 +0100

    Implemented a function 'getPokeData' to generate a random Pokemon ID between 1 and 150, and construct the final PokeAPI URL for fetching Pokemon data.

--- scripts.js
+++ scripts.js
@@ -4,4 +4,14 @@ const url = " https://pokeapi.co/api/v2/pokemon/";
 const card = document.getElementById("card");
 const btn = document.getElementById("btn");
 
+let getPokeData = () => {
+  // Generate a random number between 1 and 150;
+  let id = Math.floor(Math.random() * 150) + 1;
+  console.log(id);
+
+  // Combine the PokeAPI url with Pokemon id;
+  const finalUrl = url + id;
+  console.log(finalUrl);
+};
+
 btn.addEventListener("click", getPokeData);
